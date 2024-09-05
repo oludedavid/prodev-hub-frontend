c41d905319c7c56a0fad420ad14a9aa3d1ded976
Olude david <odavidolumide@gmail.com>
2024-09-05 14:36:32 +0200

    solving display name error encoutered during deployment

--- src/components/custom-component/authComponent/withRole.tsx
+++ src/components/custom-component/authComponent/withRole.tsx
@@ -1,50 +1,45 @@
-"use client";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { useEffect, useState, ComponentType } from "react";
 import Cookies from "universal-cookie";
 
-// Define the types for the HOC
 const withRole = <P extends object>(
   WrappedComponent: ComponentType<P>,
   allowedRoles: string[]
 ) => {
-  return (props: P) => {
+  const HOC = (props: P) => {
     const router = useRouter();
     const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
-    const [isLoading, setIsLoading] = useState(true);
     const cookies = new Cookies();
 
     useEffect(() => {
-      // Perform client-side role check
       const token = cookies.get("TOKEN");
       const user = cookies.get("USER");
       const userRole = user?.role;
 
-      // Redirect to login if no token is found
       if (!token) {
         router.push("/login");
         return;
       }
 
-      // Check if the user's role is allowed
       if (!allowedRoles.includes(userRole)) {
         router.push("/403");
         return;
       }
 
-      // Set authorization to true and stop loading
       setIsAuthorized(true);
-      setIsLoading(false);
-    }, [router, allowedRoles, cookies]);
+    }, [router]);
 
-    // Render loading while waiting for auth check
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
+    if (!isAuthorized) return <div>Loading...</div>;
 
-    // Render the component if authorized
-    return isAuthorized ? <WrappedComponent {...props} /> : null;
+    return <WrappedComponent {...props} />;
   };
+
+  // Adding displayName to the HOC
+  HOC.displayName = `withRole(${
+    WrappedComponent.displayName || WrappedComponent.name || "Component"
+  })`;
+
+  return HOC;
 };
 
 export default withRole;
